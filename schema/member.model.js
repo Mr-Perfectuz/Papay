f44a8cdd57a7fdf5f6e5dc5d122f9b5e2c424549
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2023-10-14 07:44:19 +0900

    feat: fix member.model file

--- schema/member.model.js
+++ schema/member.model.js
@@ -1,86 +1,88 @@
 const mongoose = require("mongoose");
-const memberSchema = new mongoose.Schema({
-  mb_nick: {
-    type: String,
-    required: true,
-    index: { unique: true, sparse: true },
-  },
-  mb_phone: {
-    type: String,
-    required: true,
-  },
-  mb_password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-  mb_type: {
-    type: String,
-    required: false,
-    default: "ACTIVE",
-    default: "USER",
-    enum: {
-      values: member_status_enums,
-      message: "{VALUE} is not permitted values",
+const memberSchema = new mongoose.Schema(
+  {
+    mb_nick: {
+      type: String,
+      required: true,
+      index: { unique: true, sparse: true },
+    },
+    mb_phone: {
+      type: String,
+      required: true,
+    },
+    mb_password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    mb_type: {
+      type: String,
+      required: false,
+      default: "ACTIVE",
+      default: "USER",
+      enum: {
+        values: member_status_enums,
+        message: "{VALUE} is not permitted values",
+      },
+    },
+    mb_status: {
+      type: String,
+      required: false,
+      default: "ACTIVE",
+      default: "USER",
+      enum: {
+        values: member_status_enums,
+        message: "{VALUE} is not permitted values",
+      },
+    },
+    mb_adress: {
+      type: String,
+      required: false,
+    },
+    mb_description: {
+      type: String,
+      required: false,
+    },
+    mb_image: {
+      type: String,
+      required: false,
+    },
+    mb_point: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    mb_top: {
+      type: String,
+      required: false,
+      default: "N",
+      enum: {
+        value: ordinary_enums,
+        message: "{VALUE} is not permitted values",
+      },
+    },
+    mb_views: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    mb_likes: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    mb_follow_cnt: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    mb_subscriber_cnt: {
+      type: Number,
+      required: false,
+      default: 0,
     },
   },
-  mb_status: {
-    type: String,
-    required: false,
-    default: "ACTIVE",
-    default: "USER",
-    enum: {
-      values: member_status_enums,
-      message: "{VALUE} is not permitted values",
-    },
-  },
-  mb_adress: {
-    type: String,
-    required: false,
-  },
-  mb_description: {
-    type: String,
-    required: false,
-  },
-  mb_image: {
-    type: String,
-    required: false,
-  },
-  mb_point: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  mb_top: {
-    type: String,
-    required: false,
-    default: "N",
-    enum: {
-      value: ordinary_enums,
-      message: "{VALUE} is not permitted values",
-    },
-  },
-  mb_views: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  mb_likes: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  mb_follow_cnt: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  mb_subscriber_cnt: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-   {timestamps: true}
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Member", memberSchema);
