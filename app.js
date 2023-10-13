5cda51a80e98bf6e714ccb695601920a4c793f3e
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2023-10-13 13:05:16 +0900

    fix: mongoose connection is created

--- app.js
+++ app.js
@@ -3,23 +3,19 @@ const express = require("express");
 const app = express();
 const router = require("./router");
 
-// MongDB chaqirish
-const db = require("./server").db();
-const mongodb = require("mongodb");
-
 // Har qanday browserdan kelayotgan requestlar uchun public folder ochiq degani
-app.use(express.static("public"));
 
+//2 Kirish codlari
+app.use(express.static("public"));
 //json formatdagi data ni objectga exchage qilish
 app.use(express.json());
-
 // html formdan qabul qilinadigan data larni serverga kiritish uchun
 app.use(express.urlencoded({ extended: true }));
 
 //2 Session codlari
 
 //3 Views codelari
-//BSSR
+
 // ejs orqali backend ni ichida frontend ni yashash
 app.set("views", "views");
 app.set("view engine", "ejs");
--- server.js
+++ server.js
@@ -2,23 +2,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const http = require("http");
-const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
-let db;
+mongoose.set("strictQuery", false);
 const connectionString = process.env.MONGO_URL;
 
-mongodb.connect(
+mongoose.connect(
   connectionString,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
-  (err, client) => {
+  (err, goose) => {
     if (err) console.log("Error on connection MongoDb");
     else {
       console.log("MongoDB connection succeed");
-      module.exports = client;
-      // console.log(client);
+      // console.log(goose);
       const app = require("./app");
       const server = http.createServer(app);
 
