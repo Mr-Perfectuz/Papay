3bb49194a32c9d9d0304e923c98068e83d77cbd4
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2023-10-17 16:54:39 +0900

    feat: member login process development

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -7,16 +7,24 @@ memberController.signup = async (req, res) => {
     console.log("POST: cont/signup");
     const data = req.body;
     console.log("bodu::", req.body);
-    const member = new Member();
-    const new_member = await member.signupData(data);
+    (member = new Member()), (new_member = await member.signupData(data));
     res.json({ state: "succeed", data: new_member });
   } catch (err) {
     res.json({ state: "failed", message: err });
   }
 };
-memberController.login = (req, res) => {
-  console.log(" POST cont.login");
-  res.send("login sahifasidasiz");
+memberController.login = async (req, res) => {
+  try {
+    console.log("POST: cont/login");
+    const data = req.body;
+    console.log("bodu::", req.body),
+      (member = new Member()),
+      (result = await member.loginDate(data));
+    res.json({ state: "succeed", data: result });
+  } catch (err) {
+    // console.log("ERROR: cont/login", err.message);
+    res.json({ state: "failed", message: err.message });
+  }
 };
 memberController.logout = (req, res) => {
   console.log(" GET cont.logout");
