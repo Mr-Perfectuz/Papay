91b568a9a146bd222c0b39e11645229fcd3165fe
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2023-10-21 19:26:20 +0900

    fix: modify image uploader utils

--- router_bssr.js
+++ router_bssr.js
@@ -4,7 +4,8 @@ const express = require("express");
 const router_bssr = express.Router();
 const restaurantController = require("./controllers/restaurantController");
 const productController = require("./controllers/productController");
-const { uploadProductImage } = require("./utils/uploadMulter");
+const uploader_product = require("./utils/uploadMulter");
+const productUploader = uploader_product("products");
 
 /*           BSSR EJS     */
 
@@ -24,7 +25,7 @@ router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
 router_bssr.post(
   "/products/create",
   restaurantController.validateAuthRestaurant,
-  uploadProductImage.single("product_image"),
+  productUploader.array("product_images", 5), // Use productUploader here
   productController.addNewProduct
 );
 router_bssr.post("/products/edit/:id", productController.updateChosenProduct);
--- utils/uploadMulter.js
+++ utils/uploadMulter.js
@@ -2,17 +2,25 @@ const path = require("path");
 const multer = require("multer");
 const uuid = require("uuid");
 
-const product_storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/products");
-  },
-  filename: function (req, file, cb) {
-    console.log(file);
-    const extention = path.parse(file.originalname).ext;
-    const random_name = uuid.v4() + extention;
+// Multiple image uploader
+function getTargetImageStorage(address) {
+  return multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, `./uploads/${address}`);
+    },
+    filename: function (req, file, cb) {
+      console.log(file);
+      const extention = path.parse(file.originalname).ext;
+      const random_name = uuid.v4() + extention;
 
-    cb(null, random_name);
-  },
-});
+      cb(null, random_name);
+    },
+  });
+}
 
-module.exports.uploadProductImage = multer({ storage: product_storage });
+const makeUploader = (address) => {
+  const storage = getTargetImageStorage(address);
+  return multer({ storage: storage });
+};
+
+module.exports = makeUploader;
